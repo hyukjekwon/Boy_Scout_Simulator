1c59a2bfb5435399ee775c7a2d8accbe61e82db7
BrandonHalig <brandonhalig@gmail.com>
2022-12-21 09:12:49 -0500

    error fix

--- Assets/GameInteractions.cs
+++ Assets/GameInteractions.cs
@@ -39,6 +39,7 @@ public class GameInteractions : MonoBehaviour
     {
         // Directions that get displayed in the notepad depending on the objective the player needs to find
         notesText = new Dictionary<int, string>();
+        notesText.Add(-1, "Notes");
         notesText.Add(0, "1.) Start at the\nlamppost\n\n2.) 5° 29 paces\n\n3.) 15° 20 paces\n\n4.) 320° 49 paces\n\n5.) 350° 13 paces");
         notesText.Add(1, "1.) 260° 25 paces\n\n2.) 315° 70 paces\n\n3.) 15° 25 paces\n\n4.) 90° 40 paces\n\n5.) 120° 80 paces");
         notesText.Add(2, "1.) 345° 79 paces\n\n2.) 300° 25 paces");
